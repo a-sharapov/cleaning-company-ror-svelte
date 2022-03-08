711275dad97dcb19f095ebfa8577a402826e26bc
Alexander Sharapov <alexander@sharapov.tech>
2022-03-08 17:03:02 +0300

    add node adapter for HMR

--- frontend/src/lib/components/UI/Toasts.svelte
+++ frontend/src/lib/components/UI/Toasts.svelte
@@ -1,11 +1,18 @@
 <script>
   export let message
   export let type
+
+  let currentMessage = writable($message)
+
+  const handleOnClick = (event) => {
+    event.target.setAttribute("data-hidden", true)
+    currentMessage.set("")
+  }
 </script>
 
-{#if message}
+{#if currentMessage}
   <dialog class="toast" data-type="{type}" data-hidden="false" on:click={handleOnClick}>
     <p class="toast-title">Уведомляем!</p>
-    <p>{message}</p>
+    <p>{currentMessage}</p>
   </dialog>
 {/if}
